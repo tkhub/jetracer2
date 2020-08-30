a28fcf8cf3061ea6b1ef00c9d43a7d00527d02fd
Taka.I <takap.colonel@gmail.com>
2020-08-31 01:22:16 +0900

    録画ボタン機能大体OK

--- jetracer_cnn/scripts/jetracer_rec.py
+++ jetracer_cnn/scripts/jetracer_rec.py
@@ -15,7 +15,19 @@ def btn_th(self):
         recst = "recpre"
     else:
         recst = "recgo"
-    print(recst_bf + "->" + recst)
+
+def recloop(camera, filepath, intv):
+    global recst
+    while recst == "recgo":
+        img = camera.read()
+        filename = filepath + '%d_%d_%s.jpg' % (0, 0, str(uuid.uuid1()))
+        cv2.imwrite(filename, img)
+        time.sleep(intv)
+
+def waitrec():
+    global recst
+    while recst == "recpre":
+        pass
 
 def execute():
     global recst
@@ -24,18 +36,15 @@ def execute():
     GPIO.add_event_detect(21,GPIO.FALLING, callback=btn_th, bouncetime=200)
 
     camera0 = nano.Camera(flip=2, width=224, height=224, fps=30)
-    #for i in range(100):
-    while recst == "recpre":
-        pass
-    while recst == "recgo":
-        img = camera0.read()
-        #print(sw)
-        # 21
-        filename = './data/apex/%d_%d_%s.jpg' % (0, 0, str(uuid.uuid1()))
-        #filename = './data/apex/%d.jpg' % (i)
-        cv2.imwrite(filename, img)
-        time.sleep(0.1)
+    # wait rec button
+    print("REC wait ...")
+    waitrec()
+    # rec start
+    print("REC START! ...")
+    recloop(camera0, "./data/apex/", 0.1)
+    print("REC END!! ...")
     camera0.release()
+    # finish
     del camera0
     GPIO.cleanup()
 
