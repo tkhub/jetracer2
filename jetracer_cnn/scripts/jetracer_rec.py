e8bb7f775defbb1836dcd98562770c47333dbf64
Taka.I <takap.colonel@gmail.com>
2020-08-31 01:03:44 +0900

    ボタンを押すことで録画を開始と停止

--- jetracer_cnn/scripts/jetracer_rec.py
+++ jetracer_cnn/scripts/jetracer_rec.py
@@ -1,10 +1,46 @@
 #!/usr/bin/env python3
 import cv2
 import nanocamera as nano
+import Jetson.GPIO as GPIO
+import uuid
+import time
+
+recst = "recpre"
+def btn_th(self):
+    global recst
+    recst_bf = recst
+    if recst == "recgo":
+        recst = "recend"
+    elif recst == "recend":
+        recst = "recpre"
+    else:
+        recst = "recgo"
+    print(recst_bf + "->" + recst)
 
 def execute():
-    camera0 = nano.Camera(flip=2, width=224, hight=224, fps=30)
-    
+    global recst
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(21, GPIO.IN)
+    GPIO.add_event_detect(21,GPIO.FALLING, callback=btn_th, bouncetime=200)
+
+    camera0 = nano.Camera(flip=2, width=224, height=224, fps=30)
+    #for i in range(100):
+    while recst == "recpre":
+        pass
+    while recst == "recgo":
+        img = camera0.read()
+        #print(sw)
+        # 21
+        filename = './data/apex/%d_%d_%s.jpg' % (0, 0, str(uuid.uuid1()))
+        #filename = './data/apex/%d.jpg' % (i)
+        cv2.imwrite(filename, img)
+        time.sleep(0.1)
+    camera0.release()
+    del camera0
+    GPIO.cleanup()
+
+
+
 
 
 if __name__ == '__main__':
