f029dfcd1e6513961a1e7f6daa475e9da8707e54
tkhub <takap.colonel@gmail.com>
2020-09-06 14:29:08 +0900

    B01ボードのDualCameraに対応

--- jetracer_cnn/scripts/jetracer_rec.py
+++ jetracer_cnn/scripts/jetracer_rec.py
@@ -32,6 +32,20 @@ def recloop(camera, filepath, intv):
         cv2.imwrite(filename, img)
         time.sleep(intv)
 
+def recloopDual(cameraL, cameraR, filepathL, filepathR, intv):
+    global recst
+    while recst == "recgo":
+        #img = camera.read()
+        imgL = cameraL.read()
+        imgR = cameraR.read()
+        s_uuid = str(uuid.uuid1()
+        # filename = filepath + '%d_%d_%s.jpg' % (0, 0, s_uuid))
+        filenameL = filepathL + '%d_%d_%s.jpg' % (0, 0, s_uuid))
+        filenameR = filepathR + '%d_%d_%s.jpg' % (0, 0, s_uuid))
+        cv2.imwrite(filenameL, imgL)
+        cv2.imwrite(filenameR, imgR)
+        time.sleep(intv)
+
 def testloop():
     global recst
     cnt = 0
@@ -54,14 +68,19 @@ def execute():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(recbtn, GPIO.IN)
     GPIO.add_event_detect(recbtn,GPIO.FALLING, callback=btn_th, bouncetime=200)
-
+    phpathL = "./data/apexL/"
+    phpathR = "./data/apexR/"
+    # Left Camera
     camera0 = nano.Camera(device_id=0, flip=2, width=224, height=224, fps=30)
+    # Right Camera
+    camera1 = nano.Camera(device_id=1, flip=2, width=224, height=224, fps=30)
     # wait rec button
     print("REC wait ...")
     waitrec()
     # rec start
     print("REC START! ...")
-    recloop(camera0, "./data/apex/", 0.1)
+#    recloop(camera0, "./data/apex/", 0.1)
+    recloopDual(camera0,camera1, phpathL,phpathR, 0.1)
     # testloop()
     print("REC END!! ...")
     camera0.release()
