2aeed2e24300b8fe905695265bba09794aee598f
tkhub <takap.colonel@gmail.com>
2020-09-06 14:45:01 +0900

    ステレオカメラとして合成（多分動くはず）

--- jetracer_cnn/scripts/jetracer_rec.py
+++ jetracer_cnn/scripts/jetracer_rec.py
@@ -46,6 +46,22 @@ def recloopDual(cameraL, cameraR, filepathL, filepathR, intv):
         cv2.imwrite(filenameR, imgR)
         time.sleep(intv)
 
+def recloopStereo(cameraL, cameraR, filepathMx, filepathL, filepathR, intv):
+    global recst
+    while recst == "recgo":
+        #img = camera.read()
+        imgL = cameraL.read()
+        imgR = cameraR.read()
+        s_uuid = str(uuid.uuid1()
+        # filename = filepath + '%d_%d_%s.jpg' % (0, 0, s_uuid))
+        filenameMx = filepathMx +'%d_%d_%s.jpg' % (0, 0, s_uuid))
+        filenameL = filepathL + '%d_%d_%s.jpg' % (0, 0, s_uuid))
+        filenameR = filepathR + '%d_%d_%s.jpg' % (0, 0, s_uuid))
+        cv2.imwrite(filenameL, imgL)
+        cv2.imwrite(filenameR, imgR)
+        imgMx = cv2.addWeighted(src1 = imgL, alpha=0.5, src2 = imgR, beta = 0.5, gamma = 0)
+        time.sleep(intv)
+
 def testloop():
     global recst
     cnt = 0
@@ -70,6 +86,10 @@ def execute():
     GPIO.add_event_detect(recbtn,GPIO.FALLING, callback=btn_th, bouncetime=200)
     phpathL = "./data/apexL/"
     phpathR = "./data/apexR/"
+    phpathMx = "./data/apex/"
+    os.makedirs(phpathL, exist_ok = True)
+    os.makedirs(phpathR, exist_ok = True)
+    os.makedirs(phpathMx, exist_ok = True)
     # Left Camera
     camera0 = nano.Camera(device_id=0, flip=2, width=224, height=224, fps=30)
     # Right Camera
@@ -80,7 +100,8 @@ def execute():
     # rec start
     print("REC START! ...")
 #    recloop(camera0, "./data/apex/", 0.1)
-    recloopDual(camera0,camera1, phpathL,phpathR, 0.1)
+#    recloopDual(camera0,camera1, phpathL,phpathR, 0.1)
+    recloopStereo(camera0,camera1, phpathMx, phpathL,phpathR, 0.1)
     # testloop()
     print("REC END!! ...")
     camera0.release()
