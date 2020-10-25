e665c6350223dc6cf789820f5e16fc2835ab06bb
tkhub <takap.colonel@gmail.com>
2020-10-25 11:12:24 +0900

    録画・教示系を刷新する準備で録画側を修正

--- jetracer_cnn/scripts/jetracer_rec.py
+++ jetracer_cnn/scripts/jetracer_rec.py
@@ -28,7 +28,7 @@ def btn_th(self):
         recst = "recgo"
 
 # 録画用ループ関数
-def recloop(camera, filepath, intv):
+def recloop_old(camera, filepath, intv):
     global recst
     while recst == "recgo":
         img = camera.read()
@@ -36,7 +36,7 @@ def recloop(camera, filepath, intv):
         cv2.imwrite(filename, img)
         time.sleep(intv)
 
-def recloopDual(cameraL, cameraR, filepathL, filepathR, intv):
+def recloopDual_old(cameraL, cameraR, filepathL, filepathR, intv):
     global recst
     while recst == "recgo":
         #img = camera.read()
@@ -50,7 +50,38 @@ def recloopDual(cameraL, cameraR, filepathL, filepathR, intv):
         cv2.imwrite(filenameR, imgR)
         time.sleep(intv)
 
-def recloopStereo(cameraL, cameraR, filepathMx, filepathL, filepathR, intv):
+def recloopDual(cameraL, cameraR, filepathL, filepathR, intv):
+    global recst
+    while recst == "recgo":
+        #img = camera.read()
+        imgL = cameraL.read()
+        imgR = cameraR.read()
+        timestmp = time.time()
+        # filename = filepath + '%d_%d_%s.jpg' % (0, 0, s_uuid))
+        filenameL = filepathL + str(timestmp) + '.jpg'
+        filenameR = filepathR + str(timestmp) + '.jpg'
+        cv2.imwrite(filenameL, imgL)
+        cv2.imwrite(filenameR, imgR)
+        time.sleep(intv)
+
+def recloopStereo_old(cameraL, cameraR, filepathMx, filepathL, filepathR, intv):
+    global recst
+    while recst == "recgo":
+        #img = camera.read()
+        imgL = cameraL.read()
+        imgR = cameraR.read()
+        s_uuid = str(uuid.uuid1())
+        # filename = filepath + '%d_%d_%s.jpg' % (0, 0, s_uuid))
+        filenameMx = filepathMx +'%d_%d_%s.jpg' % (0, 0, s_uuid)
+        filenameL = filepathL + '%d_%d_%s.jpg' % (0, 0, s_uuid)
+        filenameR = filepathR + '%d_%d_%s.jpg' % (0, 0, s_uuid)
+        cv2.imwrite(filenameL, imgL)
+        cv2.imwrite(filenameR, imgR)
+        imgMx = cv2.addWeighted(src1 = imgL, alpha=0.5, src2 = imgR, beta = 0.5, gamma = 0)
+        cv2.imwrite(filenameMx, imgMx)
+        time.sleep(intv)
+
+def recloopStereo_old(cameraL, cameraR, filepathMx, filepathL, filepathR, intv):
     global recst
     while recst == "recgo":
         #img = camera.read()
@@ -90,23 +121,26 @@ def execute():
     GPIO.setup(recbtn, GPIO.IN)
     GPIO.add_event_detect(recbtn,GPIO.FALLING, callback=btn_th, bouncetime=200)
     phpathL = "./data/apexL/"
+#    phpathL = "./data/recR/"
     phpathR = "./data/apexR/"
+#    phpathR = "./data/recR/"
     phpathMx = "./data/apexM/"
     os.makedirs(phpathL, exist_ok = True)
     os.makedirs(phpathR, exist_ok = True)
     os.makedirs(phpathMx, exist_ok = True)
     # Left Camera
-    camera0 = nano.Camera(device_id=0, flip=2, width=224, height=224, fps=30)
+    camera0 = nano.Camera(device_id=0, flip=2, width=224, height=224, fps=60)
     # Right Camera
-    camera1 = nano.Camera(device_id=1, flip=2, width=224, height=224, fps=30)
+    camera1 = nano.Camera(device_id=1, flip=2, width=224, height=224, fps=60)
     # wait rec button
     print("REC wait ...")
     waitrec()
     # rec start
     print("REC START! ...")
-#    recloop(camera0, "./data/apex/", 0.1)
-#    recloopDual(camera0,camera1, phpathL,phpathR, 0.1)
-    recloopStereo(camera0,camera1, phpathMx, phpathL,phpathR, 0.1)
+#    recloop_old(camera0, "./data/apex/", 0.1)
+#    recloopDual_old(camera0,camera1, phpathL,phpathR, 0.1)
+    recloopStereo_old(camera0,camera1, phpathMx, phpathL,phpathR, 0.05)
+#    recloopDual(camera0,camera1, phpathL,phpathR, 0.05)
     # testloop()
     print("REC END!! ...")
     camera0.release()
